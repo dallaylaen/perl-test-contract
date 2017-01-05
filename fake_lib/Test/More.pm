4f18587461be79faf323586c2255a96f7ee6f475
Konstantin S. Uvarin <khedin@gmail.com>
2017-01-05 02:52:10 +0200

    TST Add a fake Test::More to be able to check compatibility quick

---
+++ fake_lib/Test/More.pm
@@ -0,0 +1,28 @@
+package Test::More;
+
+use strict;
+use warnings;
+our $VERSION = 0.01;
+
+=head1 STOP!
+
+This is a fake Test::More.
+It is only here to test how Test::Refute behaves in place of Test::More
+without rewriting a lot of test scripts.
+
+=cut
+
+use Test::Refute;
+use Carp;
+
+# Just re-export everything
+our @ISA = qw(Test::Refute Exporter);
+our @EXPORT = @Test::Refute::EXPORT;
+
+END {
+    if (Test::Builder->can("import")) {
+        carp "Real Test::More loaded, the script may misbehave";
+    };
+};
+
+1;
