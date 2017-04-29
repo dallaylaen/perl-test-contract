2ca7b599fa231284750cf25f4837191700377f28
Konstantin S. Uvarin <khedin@gmail.com>
2017-04-30 01:12:45 +0300

    DOC Fixup pod in T::C

--- lib/Test/Contract.pm
+++ lib/Test/Contract.pm
@@ -2,27 +2,35 @@ package Test::Contract;
 
 use strict;
 use warnings;
-our $VERSION = 0.0209;
+our $VERSION = 0.0210;
 
 =head1 NAME
 
-Test::Contract - apply a series of tests/assertions within an application/module
+Test::Contract - Object-oriented testing and assertion tool
 
 =head1 SYNOPSIS
 
-    package My::Module;
+    # Somewhere inside production codebase
     use Test::Contract;
 
-    sub my_method {
-        my $user_data = shift;
-
-        my $contract = Test::Contract->new;
-        $contract->is( $user_data->answer, 42, "Life and everything" );
-        $contract->done_testing;
-        if ($contract->get_passing) {
-            ...
-        };
+    my $c = Test::Contract->new;
+    $c->is ($foo, $bar, "Foo equals bar");
+    $c->like ($user_input, qr/F?o?r?m?a?t/, "Input format as expected");
+    if ($c->get_passing) {
+        warn $c->get_tap;
     };
+
+=head1 DESCRIPTION
+
+Test::Contract can check sets of L<Test::More>-like conditions
+without turning the whole application into a giant test script.
+
+This may be useful when validating user input, loading plug-ins or external
+modules, or checking that the code is error-free.
+
+A counterpart method exists for ALL Test::More standard checks.
+
+Also it is fairly simple to build custom conditions.
 
 =cut
 
@@ -33,9 +41,10 @@ use Exporter qw(import);
 use Test::Contract::Engine::Build qw(to_scalar);
 
 our @EXPORT_OK = qw(contract);
-our @CARP_NOT = qw(Test::Contract::Engine::Build Test::Contract);
+our @CARP_NOT = qw(Test::Contract::Engine::Build Test::Contract::Unit);
 # preload most basic tests
 require Test::Contract::Basic;
+require Test::Contract::Basic::Deep;
 
 =head1 OPTIONAL EXPORT
 
@@ -596,11 +605,9 @@ sub diag {
     return;
 };
 
-=head2 note( $message )
+=head2 note( @message )
 
 Add a side note warning message.
-
-The interface MAY change in the future in favour of @list.
 
 =cut
 
@@ -651,5 +658,90 @@ sub _log {
     push @{ $self->{log} }, $mess;
 };
 
+=head1 AUTHOR
 
-1;
+Konstantin S. Uvarin, C<< <khedin at gmail.com> >>
+
+=head1 BUGS
+
+This is alpha software, lots of bugs guaranteed.
+
+Please report any bugs or feature requests to C<bug-test-refute at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-Contract>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Test::Contract
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Test-Contract>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Test-Contract>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Test-Contract>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Test-Contract/>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+Karl Popper (the philosopher) inspired me to invert assertion into refutation.
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2016 Konstantin S. Uvarin.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=cut
+
+1; # End of Test::Contract
