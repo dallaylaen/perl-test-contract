6ec838588ce06171e4f7dc930d2e589f57dca6af
Konstantin S. Uvarin <khedin@gmail.com>
2017-05-02 03:30:55 +0300

    BIN Add -m,-M options to refute.pl

--- bin/refute.pl
+++ bin/refute.pl
@@ -4,6 +4,7 @@ package Test::Contract::bin; # avoid polluting main
 
 use strict;
 use warnings;
+use Carp;
 use File::Find;
 use Getopt::Long;
 use File::Basename qw(basename dirname);
@@ -19,10 +20,13 @@ my $all = Test::Contract::Engine::TAP->new;
 @ARGV = map { /^(-[IMm])(.*)/ ? ($1, $2) : $_ } @ARGV;
 
 my @inc;
+my @use;
 my $fake;
 my @preload;
 GetOptions (
     "I=s" => \@inc,
+    "M=s" => sub { push @use, $_[0] =~ /=/ ? $_[0] : "$_[0]=" },
+    "m=s" => \@use,
     "help" => \&usage,
     "preload=s" => \@preload,
     "faketest" => \$fake,
@@ -33,7 +37,9 @@ sub usage {
 $0 [options] [test_dir] ...
 - A TAP-based test suite runner
 Options may include:
-    -I - add a library path
+    -I - add a library path (see perlrun)
+    -M[-]Module=arg,...
+    -m[-]Module - load/unload modules with/without args (see perlrun)
     --preload Module::Name,Other::Module... - experimental module caching mode
     --faketest (EXPERIMENTAL) - use Test::Contract in place of Test::More
     --help - this message
@@ -44,6 +50,7 @@ HELP
 
 my @plopt;
 push @plopt, map { "-I$_" } @inc;
+push @plopt, map { /=/ ? "-M$_" : "-m$_" } @use;
 @preload = split /,/,join ",", @preload;
 
 if ($fake) {
@@ -72,12 +79,13 @@ if (@preload) {
 
     package main;
 
-    foreach my $mod( @preload ) {
-        my $fname = $mod;
-        $fname =~ s#::#/#g;
-        $fname .= ".pm" unless $fname =~ /\.p[lm]$/;
-        require $fname;
-        warn "Preloaded $mod from $INC{$fname}\n";
+    foreach ( @use ) {
+        my ($mod, $rawarg) = split /=/, $_, 2;
+        my $arg = defined $rawarg ? [split /,/, $rawarg ] : undef;
+        Test::Contract::bin::load_module($mod, $arg);
+    };
+    foreach ( @preload ) {
+        Test::Contract::bin::load_module($_);
     };
 };
 
@@ -141,5 +149,26 @@ sub get_reader {
         return Test::Contract::Engine::TAP::Reader->new (
             indent => 1, exec => [ perl => @plopt => @localopt => $f ] );
     };
+};
+
+sub load_module {
+    my ($mod, $args) = @_;
+    $mod =~ /^(-?)([a-z]\w+(?:::\w+)*)$/
+        or croak "Bad module name: $mod";
+    (my $no, $mod) = ($1, $2);
+    my $file = $mod;
+    $file =~ s#::#/#g;
+    $file .= ".pm";
+    eval { require $file; 1 } or croak $@;
+    if ($no) {
+        $args ||= [];
+        eval { $mod->unimport(@$args); 1; }
+            or croak "Failed to unload $mod: $@";
+    }
+    elsif ($args) {
+        eval { $mod->import(@$args); 1; }
+            or croak "Failed to preload $mod: $@";
+    };
+    warn( ($no ? "Pre":"Un")."loaded $mod from $INC{$file}\n");
 };
 
