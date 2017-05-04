637ae8f414e696d10f06e541c4a89652d7be96ea
Konstantin S. Uvarin <khedin@gmail.com>
2017-05-04 06:10:42 +0300

    MOD Add some crude T::C::Warn with warns_like {};

---
+++ lib/Test/Contract/Warn.pm
@@ -0,0 +1,68 @@
+package Test::Contract::Warn;
+
+use strict;
+use warnings;
+our $VERSION = 0.0301;
+
+=head1 NAME
+
+Test::Contract::Warn - make sure a BLOCK warns exactly as expected
+
+=head1 SYNOPSIS
+
+    use Test::More;
+    use Test::Contract::Warn;
+
+    warns_like {
+        warn "food";
+        warb "bard";
+    } [qr/foo/, qr/bar/];
+
+    done_testing;
+
+=head1 FUNCTIONS
+
+All functions here are exported by default.
+
+=cut
+
+use Exporter qw(import);
+
+use Test::Contract::Engine::Build;
+
+=head2 warns_like { CODE; } ["warn", qr/or warn/], "Explanation";
+
+Check that a code block warns in exactly the specified manner.
+
+Unexpected number of warnings, failure to match patterns, or changed order
+would all result in a failing test.
+
+This code works by setting C<$SIG{__WARN__}>, so if the code under test
+does it as well this won't work.
+
+=cut
+
+build_refute warns_like => sub {
+    my ($code, $rex) = @_;
+
+    $rex ||= [];
+
+    my @warn;
+    local $SIG{__WARN__} = sub { push @warn, $_[0] };
+    eval { $code->(); 1 }
+        or return "warns_like{}: unhandled exception: $@";
+
+    if (@warn == @$rex) {
+        my @bad;
+        for( my $i = 0; $i < @$rex; $i++ ) {
+            $warn[$i] =~ /$rex->[$i]/
+                or push @bad, "Warning[$i] '$warn[$i]' doesn't match qr/$rex->[$i]/";
+        };
+        return join "\n", @bad;
+    } else {
+        return sprintf "Got %u warnings when %u expected; warns:\n%s",
+            scalar @warn, scalar @$rex, join "\n", @warn;
+    };
+}, block => 1, args => 1, export => 1;
+
+1;
---
+++ t/402-warn.t
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::Contract::Unit;
+use Test::Contract::Warn;
+
+my $c;
+
+$c = contract {
+    warns_like {
+        warn "Foo";
+        warn "Bar";
+    } ["Foo", qr/Ba/], "all ok";
+};
+
+contract_is $c, "1", "warnings ok";
+note $c->get_tap;
+
+$c = contract {
+    warns_like {
+        warn "Foo";
+    } ["Foo", qr/Ba/], "all ok";
+};
+
+contract_is $c, "0", "warnings count mismatch";
+note $c->get_tap;
+
+$c = contract {
+    warns_like {
+        warn "Bar";
+        warn "Foo";
+    } ["Foo", qr/Ba/], "all ok";
+};
+
+contract_is $c, "0", "warnings bad order";
+note $c->get_tap;
+
+done_testing;
